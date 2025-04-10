0e1bfec22650179f464e4d552cca9eabeff3f16e
Maria Lazutkina <lazutkina2005maria@mail.ru>
2025-04-10 21:43:45 +0300

    ..

---
+++ git/vk-edu-web-hw-new/app/views.py
@@ -0,0 +1,149 @@
+from django.shortcuts import render, get_object_or_404
+from copy import deepcopy
+from django.core.paginator import Paginator
+from django.http import Http404
+from django.shortcuts import redirect
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login, logout
+from django.contrib.auth.decorators import login_required
+
+QUESTIONS = [
+    {
+        'title': f'Title {i}',
+        'id': i,
+        'text': f'Text for question #{i}',
+        'img_path': "img/ava.jpg",  
+        'answers_count': 0, 
+        'likes': 0,
+        'tags': ['django', 'python'],
+        'answers': [
+        {'user': 'blablabla', 'text': 'text blablabla'},
+        {'user': 'blebleble', 'text': 'text blebleble'},
+        ]
+    } for i in range(1, 31)  
+]
+
+answers = [
+    {
+        'user': {'username': 'john_doe', 'avatar': 'path_to_avatar'},  
+        'text': 'This is an answer to the question.',
+        'likes': 10,
+    },
+]
+
+
+def get_all_tags():
+    return {tag for question in QUESTIONS for tag in question['tags']}
+
+def index(request):
+    paginator = Paginator(QUESTIONS, 10)  
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    all_tags = get_all_tags()  
+    return render(request, 'index.html', {'page_obj': page_obj, 'all_tags': all_tags})
+
+
+def hot(request):
+    hot_questions = sorted(deepcopy(QUESTIONS), key=lambda x: -x['id'])
+    paginator = Paginator(hot_questions, 10)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    all_tags = get_all_tags()
+    return render(request, 'hot.html', {'page_obj': page_obj, 'all_tags': all_tags})
+
+def question(request, question_id):
+    question_obj = next((q for q in QUESTIONS if q['id'] == question_id), None)
+    if question_obj is None:
+        raise Http404("Question not found")
+    
+    answers = question_obj.get('answers', [])
+    
+    paginator = Paginator(answers, 5)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    
+    tags = question_obj.get('tags', []) 
+    all_tags = get_all_tags() 
+    
+    if request.method == 'POST' and 'like_answer' in request.POST:
+        answer_id = request.POST.get('answer_id')
+        for answer in answers:
+            if answer['id'] == answer_id:
+                answer['likes'] += 1
+                break
+    
+    return render(request, 'single_question.html', {
+        'question': question_obj,
+        'page_obj': page_obj,
+        'tags': tags, 
+        'all_tags': all_tags
+    })
+
+
+def tag(request, tag_name):
+    tagged_questions = [q for q in QUESTIONS if tag_name in q['tags']]
+    paginator = Paginator(tagged_questions, 10)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    all_tags = get_all_tags() 
+    return render(request, 'tag.html', {
+        'page_obj': page_obj,
+        'tag_name': tag_name,
+        'all_tags': all_tags,
+        'selected_tag': tag_name,
+    })
+
+def like_question(request, question_id):
+    if request.method == 'POST':
+        liked_questions = request.session.get('liked_questions', [])
+
+        for q in QUESTIONS:
+            if q['id'] == question_id:
+                if question_id in liked_questions:
+                    q['likes'] -= 1
+                    liked_questions.remove(question_id)
+                else:
+                    q['likes'] += 1
+                    liked_questions.append(question_id)
+                break
+
+        request.session['liked_questions'] = liked_questions
+
+    return redirect(request.META.get('HTTP_REFERER', 'index'))
+
+def ask(request):
+    all_tags = get_all_tags()
+    return render(request, 'ask.html', {'all_tags': all_tags})
+
+def signup_view(request):
+    all_tags = get_all_tags() 
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('index')
+    else:
+        form = UserCreationForm()
+    return render(request, 'signup.html', {'form': form, 'all_tags': all_tags})
+
+def login_view(request):
+    all_tags = get_all_tags() 
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('index')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'login.html', {'form': form, 'all_tags': all_tags})
+
+def logout_view(request):
+    logout(request)
+    return redirect('index') 
+
+@login_required
+def settings_view(request):
+    all_tags = get_all_tags()  # Получаем все теги
+    return render(request, 'settings.html', {'all_tags': all_tags})
---
+++ git/vk-edu-web-hw-new/ask_lazutkina/urls.py
@@ -0,0 +1,33 @@
+"""
+URL configuration for ask_lazutkina project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from app import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', views.index, name="index"),
+    path('hot', views.hot, name="hot"),
+    path('question/<int:question_id>', views.question, name="question"),
+    path('tag/<str:tag_name>/', views.tag, name='tag'),
+    path('question/<int:question_id>/like/', views.like_question, name='like_question'),
+    path('ask/', views.ask, name='ask'),
+    path('login/', views.login_view, name='login'),
+    path('signup/', views.signup_view, name='signup'),
+    path('logout/', views.logout_view, name='logout'),
+    path('settings/', views.settings_view, name='settings'),
+]
